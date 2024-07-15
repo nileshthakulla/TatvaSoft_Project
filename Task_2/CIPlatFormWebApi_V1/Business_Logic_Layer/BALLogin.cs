e7812404ce0db2963ea46f4d262c3134ca76a683
nileshthakulla <210130107101@gecg28.ac.in>
2024-07-16 00:17:12 +0530

    added Task_2

---
+++ Task_2/CIPlatFormWebApi_V1/Business_Logic_Layer/BALLogin.cs
@@ -0,0 +1,65 @@
+﻿using Data_Logic_Layer;
+using Data_Logic_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer
+{
+    public class BALLogin
+    {
+        public DALLogin _dalLogin;
+
+        public BALLogin(DALLogin dalLogin)
+        {
+            _dalLogin = dalLogin;
+        }
+
+        ResponseResult result = new ResponseResult();
+        public ResponseResult LoginUser(User user)
+        {
+            try
+            {
+                User userObj = new User();
+                userObj = UserLogin(user);
+                if(userObj != null)
+                {
+                    if(userObj.Message.ToString() == "Login Successfully")
+                    {
+                        result.Message = userObj.Message;
+                        result.Result = ResponseStatus.Success;
+                        result.Data = userObj;
+                    }
+                    else
+                    {
+                        result.Message = userObj.Message;
+                        result.Result = ResponseStatus.Error;
+                    }
+                }
+                else
+                {
+                    result.Message = "Error in login";
+                    result.Result = ResponseStatus.Success;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public User UserLogin(User user)
+        {
+            User userObj = new User()
+            {
+                EmailAddress = user.EmailAddress,
+                Password = user.Password,
+            };
+            return _dalLogin.LoginUser(userObj); ;
+        }
+    }
+}
---
+++ Task_2/CIPlatFormWebApi_V1/CIPlatFormWebApi_V1/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Business_Logic_Layer;
+using Data_Logic_Layer.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIPlatFormWebApi_V1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public BALLogin _balLogin;
+
+        public LoginController(BALLogin balLogin)
+        {
+            _balLogin = balLogin;
+        }
+
+        ResponseResult result = new ResponseResult();
+        [HttpPost]
+        public ResponseResult LoginUser(User user)
+        {
+            try
+            {
+                result.Data = _balLogin.LoginUser(user);
+                result.Result = ResponseStatus.Success;
+            }
+            catch(Exception ex)
+            {
+                result.Message = ex.Message;
+                result.Result = ResponseStatus.Error;
+            }
+            return result;
+        }
+    }
+}
